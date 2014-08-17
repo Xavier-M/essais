9d4afd734d8a19561333448025a5239301e84489
Xav <Xav@MSIXav-PC>
2014-08-17 21:57:55 +0200

    v0

---
+++ essais/src/main/java/essais/Main.java
@@ -0,0 +1,11 @@
+package essais;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("initial");
+
+		MyClass c1 = new MyClass("init");
+	}
+
+}
---
+++ essais/src/main/java/essais/MyClass.java
@@ -0,0 +1,18 @@
+package essais;
+
+public class MyClass {
+	private String name;
+
+	public MyClass(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
